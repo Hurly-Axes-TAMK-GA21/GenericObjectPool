5c5f0e65a49e609aac5fee17547289db2db12419
Heikki G <heikki.gauffin@tuni.fi>
2021-02-24 19:55:36 +0200

    Add second object pool which uses queue

---
+++ Assets/Enemy2.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections;
+using UnityEngine;
+
+public class Enemy2 : MonoBehaviour
+{
+    private void OnEnable()
+    {
+        StartCoroutine(DeathTimer());
+    }
+
+    IEnumerator DeathTimer()
+    {
+        yield return new WaitForSeconds(5);
+
+        EnemyPool2.instance.ReturnToPool(this);
+    }
+
+    void Update()
+    {
+        transform.Translate(0,0, 1 * 3f *Time.deltaTime);
+    }
+}
